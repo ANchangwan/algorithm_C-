10afe9c9b0e2eaef3c717589d58d26ee2819318e
안창완 <ssdii66@gmail.com>
2024-05-31 20:26:22 +0900

    알파벳 개수

--- 백준/2309/2609.cpp
+++ 백준/2309/2609.cpp
@@ -15,7 +15,7 @@ int main(){
         for(int i : a) cout << i << " ";
         cout << "\n";
         int sum = 0;
-        for(int i=0; i<0; i++) sum+=a[i];
+        for(int i=0; i<7; i++) sum+=a[i];
 
         if(sum == 100)break;
     }while(next_permutation(a,a+9));
