ccccf4a2823bde982cb5bbb79bcf22fa5a6afc85
안창완 <ssdii66@gmail.com>
2024-08-11 20:09:33 +0900

    1620

--- test.cpp
+++ test.cpp
@@ -1,25 +1,26 @@
 #include<bits/stdc++.h> 
-using namespace std;   
-int N,K;
-int maxSum=0;
+using namespace std;  
+
+int n,k;
+string s;
+map<string, int> mp;
+map<int, string> mp2;
+string arr[100004];
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> N >> K;
-    vector<int> arr(N);
-    for(int i = 0; i < N; i++){
-        cin >> arr[i];
+    cin >> n >> k;
+    for(int i=0; i < n; i++){
+        cin >> s;
+        mp[s] = i + 1;
+        mp2[i+1] = s;
+        arr[i+1] = s;
     }
-    do{ 
-        int sum = 0;
-        for(int i=0; i<K; i++) {
-            sum += arr[i];
-        }
-        maxSum = max(maxSum, sum);
-        cout << maxSum << endl;
-    }while(next_permutation(arr.begin(), arr.end()));
-    cout << maxSum << endl;
+    cout << mp["hi"] << endl;
+
+    
     return 0;
 } 
 /// g++ -std=c++14 -Wall test.cpp -o test.out
--- 백준/2559/수열.cpp
+++ 백준/2559/수열.cpp
@@ -11,7 +11,8 @@ int main(){
 	cin.tie(NULL);cout.tie(NULL);
 	cin >> n >> k; 
 	for(int i = 1; i <= n; i++){
-		cin >> temp; psum[i] = psum[i - 1] + temp; 
+		cin >> temp; 
+		psum[i] = psum[i - 1] + temp; 
 	} 
 	for(int i = k; i <= n; i++){
 		ret = max(ret, psum[i] - psum[i - k]);
