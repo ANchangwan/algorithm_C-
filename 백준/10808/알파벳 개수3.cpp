1d7f98e7e6d2d7aebad98b64a708cc6d48e68646
안창완 <ssdii66@gmail.com>
2024-08-16 01:09:08 +0900

    counting star

--- 백준/10808/알파벳 개수3.cpp
+++ 백준/10808/알파벳 개수3.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int a[256];
 string s;
-map<char, int> mp;
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);cout.tie(NULL);
 	cin >> s;
-	for(int i=0; i<26; i++){
-        mp.insert({char(97+i),0});
-    }
 	for(auto i : s){
-		mp[i]++;
+		a[static_cast<unsigned char>(i)]++;
 	}
-	for(int i=0; i < 26; i++){
-        cout << mp[char(97+i)] << " ";
-    }
+	for(char i = 'a'; i<='z'; i++){
+		cout << a[static_cast<unsigned char>(i)] << " ";
+	}
+
+	
 	return 0;
 }
