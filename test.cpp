67f541e6e6fff0aa733e5c2e2f596f9fe5c426f5
안창완 <ssdii66@gmail.com>
2024-09-20 13:41:55 +0900

    사과 담기

--- test.cpp
+++ test.cpp
@@ -1,13 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std; 
-long long n,res;
+// m < n
+// 왼족 오른쪽 이동가능 하지만 스크린의 경계를 넘어가면 안된다.
+//최솟값
+
+int n, m, j, apple,l, r,ret;
+
 int main(){
-    stack<int> stk;
-	ios_base::sync_with_stdio(0); cin.tie(NULL);
-    cin >> n;
-    for(int i = 0; i< n; i++){
-        res += (n+1)*i; 
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> n >> m >> j;
+    l = 1;
+    for(int i=0;i < j; i++){
+        r = l+m-1;
+        cin >> apple;
+        if(apple >= l && apple <= r) continue;
+        if(apple < l){
+            ret+= (l - apple);
+            l = apple;
+        }else{
+            l += (apple - r);
+            ret += (apple - r);
+        }
     }
-    cout << res << "\n";
+    cout << ret << "\n";
     return 0;
 }
