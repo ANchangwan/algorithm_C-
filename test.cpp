501dc1a6780ef4b36f0d45319a3dc83b0f05b30b
안창완 <ssdii66@gmail.com>
2024-09-26 02:56:12 +0900

    test1

--- test.cpp
+++ test.cpp
@@ -1,40 +1,34 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int n,m,a[10][10],visited[10][10],ret;
+int a[10][10], visited[10][10], n, m, ret;
 vector<pair<int, int>> virusList, wallList;
-int dy[] = {-1, 0, 1,0};
-int dx[] = {0,1,0,1};
-
-
-void DFS(int y, int x){
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
+void dfs(int y, int x){
     visited[y][x] = 1;
     for(int i = 0; i < 4; i++){
         int ny = y + dy[i];
         int nx = x + dx[i];
         if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] || a[ny][nx] == 1) continue;  
-        DFS(ny, nx);
+        dfs(ny, nx);
     }
     return;
 }
-
 int solve(){
     fill(&visited[0][0], &visited[0][0] + 10 * 10, 0); 
     for(pair<int, int> b : virusList){
         visited[b.first][b.second] = 1;
-        DFS(b.first, b.second);
+        dfs(b.first, b.second);
     } 
 
-     
     int cnt = 0;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             if(a[i][j] == 0 && !visited[i][j])cnt++;
         }
     } 
-    return cnt;
+    return cnt;  
 }
-
 int main(){
     cin >> n >> m;
     for(int i = 0; i <n; i++){
