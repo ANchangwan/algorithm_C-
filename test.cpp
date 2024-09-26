d004b2b5998c4b39455f412f52157e2373732833
안창완 <ssdii66@gmail.com>
2024-09-26 22:33:36 +0900

    효율적인 해킹

--- test.cpp
+++ test.cpp
@@ -1,56 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int a[10][10], visited[10][10], n, m, ret;
-vector<pair<int, int>> virusList, wallList;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-void dfs(int y, int x){
-    visited[y][x] = 1;
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] || a[ny][nx] == 1) continue;  
-        dfs(ny, nx);
-    }
-    return;
-}
-int solve(){
-    fill(&visited[0][0], &visited[0][0] + 10 * 10, 0); 
-    for(pair<int, int> b : virusList){
-        visited[b.first][b.second] = 1;
-        dfs(b.first, b.second);
-    } 
 
-    int cnt = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(a[i][j] == 0 && !visited[i][j])cnt++;
-        }
-    } 
-    return cnt;  
+int n, m,a,b,idx,ret, visited[10004],dp[10004],mx;
+vector<int>adj[10004];
+
+int dfs(int here){
+    visited[here] = 1;
+    int ret = 1;
+    for(int there:adj[here]){
+        if(visited[there]) continue;
+        ret+= dfs(there);
+    }
+    return ret;
 }
+
+
 int main(){
     cin >> n >> m;
-    for(int i = 0; i <n; i++){
-        for(int j = 0; j < m; j++){
-            cin >> a[i][j];
-            if(a[i][j] == 2) virusList.push_back({i, j});
-            if(a[i][j] == 0) wallList.push_back({i, j});
-        }
+    for(int i=0; i <m;i++){
+        cin >> a >> b;
+        adj[b].push_back(a);
     }
-    for(int i = 0; i < wallList.size(); i++){
-        for(int j = 0; j < i; j++){
-            for(int k = 0; k < j; k++){
-                a[wallList[i].first][wallList[i].second] = 1;
-                a[wallList[j].first][wallList[j].second] = 1;
-                a[wallList[k].first][wallList[k].second] = 1;
-                ret = max(ret, solve());
-                a[wallList[i].first][wallList[i].second] = 0;
-                a[wallList[j].first][wallList[j].second] = 0;
-                a[wallList[k].first][wallList[k].second] = 0;
-            }
-        }
+    for(int i =1; i<=n; i++){
+        memset(visited, 0, sizeof(visited));
+        dp[i] = dfs(i);
+        mx = max(dp[i], mx);
     }
-    cout << ret << "\n";
+
+    
+    for (int i = 1; i <= n; i++) if(mx == dp[i]) cout << i << " "; 
+    
+    
     return 0;
 }
