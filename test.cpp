46fc6cdb91f373aec6cfd69184fb75a3e86538d4
안창완 <ssdii66@gmail.com>
2024-11-06 14:46:28 +0900

    오큰수 백준

--- test.cpp
+++ test.cpp
@@ -1,36 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,m,r,root;
-vector<int> v[54];
-bool visited[54];
-
-int dfs(int here){
-	int ret = 0;
-	// int child = 0;
-	if(v[here].empty()) return 1;
-	for(int there : v[here]){
-		if(there != r){
-			ret += dfs(there);
-			// child++;
-		}
-	}
-
-	return ret;
-}
-
-
+int n,a[1000004],ret[1000004];
+stack<int>s;
 int main(){
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 	cin >> n;
+	memset(ret,-1, sizeof(ret));
 	for(int i =0; i < n; i++){
-		cin >> m;
-		if(m == -1) root = i;
-		else v[m].push_back(i);
+		cin >> a[i];
+		while(s.size() && a[s.top()] < a[i]){
+			ret[s.top()] = a[i]; s.pop();
+		}
+		s.push(i);
 	}
-	cin >> r;
-	if(r == root){ cout << 0 << "\n";return 0;}
-
-	cout << dfs(root) << "\n";
+	for(int i =0; i < n; i++) cout << ret[i] << " ";
 	return 0;
 }
