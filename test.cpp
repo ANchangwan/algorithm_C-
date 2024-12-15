6b6cc2d4fd62d440942cabb1640bc7aa73c968fa
waney <ssdii66@gmail.com>
2024-12-16 02:14:15 +0900

    문자열 폭발

--- test.cpp
+++ test.cpp
@@ -1,22 +1,30 @@
-#include<bits/stdc++.h>
-using namespace std; 
-int n, a, b, ret; 
-vector<pair<int, int>> v; 
-priority_queue<int, vector<int>, greater<int>> pq;
+#include <bits/stdc++.h>
+using namespace std;
+
+string s1,s2;
+int n;
+
+
 int main(){
-    cin >> n; 
-    for(int i = 0; i < n; i++){
-        cin >> a >> b; v.push_back({b, a});
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> s1 >> s2;
+    while(true){
+        int n = s1.size();
+        int  i = 0;
+        bool flag = false;
+        for(i = 0; i < n;i++){
+            if(s1[i] == s2[0]){
+                if(s1.substr(i, s2.size()) == s2) {  
+                    s1.erase(i, s2.size());
+                    i += s2.size();
+                    flag = true;
+                }
+            }
+        }
+        if(!flag) break;
     }
-    sort(v.begin(), v.end());
-    for(int i = 0; i < n; i++){ 
-        pq.push(v[i].second);
-        if(pq.size() > v[i].first){ 
-            pq.pop();
-        } 
-    } 
-    while(pq.size()){
-    	ret += pq.top(); pq.pop();
-	}
-    cout << ret << "\n";
+    if(!s1.size()) cout << "FRULA" <<"\n";
+    else cout << s1 << "\n";
+    return 0;
+
 }
